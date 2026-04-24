d6354dd4795946ce3af9610b2b4c1ae841dc2605
GonzaloTT <maganagonzalosegovia@gmail.com>
2026-04-24 13:27:11 -0600

    Se corrigieron errores visuales y se agrego fuente tipografica global

---
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,9 @@
+import MainLayout from "../../components/layout/MainLayout/MainLayout";
+
+export default function Dashboard() {
+  return (
+    <MainLayout>
+      <h1 style={{ color: "white" }}>Dashboard</h1>
+    </MainLayout>
+  );
+}
