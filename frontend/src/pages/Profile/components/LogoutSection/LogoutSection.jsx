ff427989fb9f85b577e64f9c37055cff58031c85
GonzaloTT <maganagonzalosegovia@gmail.com>
2026-04-30 01:33:54 -0600

    Se agrego funcionalidad al boton cerrar sesion

--- frontend/src/pages/Profile/components/LogoutSection/LogoutSection.jsx
+++ frontend/src/pages/Profile/components/LogoutSection/LogoutSection.jsx
@@ -1,15 +1,29 @@
 import "./LogoutSection.css";
 import exitIcon from "../../../../assets/icons/exit.png";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../../../contexts/AuthContext";
 
 export default function LogoutSection() {
+  const navigate = useNavigate();
+  const { logout } = useAuth();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
+
   return (
     <section className="logout-section">
-      <button className="logout-section__button">
+      <button
+        className="logout-section__button"
+        onClick={handleLogout}
+      >
         <img
           src={exitIcon}
           alt="Cerrar sesión"
           className="logout-section__icon"
         />
+
         Cerrar Sesión
       </button>
     </section>
